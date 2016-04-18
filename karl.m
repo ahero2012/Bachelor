318d92e7eda529c4a4856a1a935db3576671a4ba
ahero2012 <aheld@student.ethz.ch>
2016-04-19 00:28:06 +0200

    Added files via upload

--- karl.m
+++ karl.m
@@ -1,12 +1,13 @@
-% clear all;clc
-files=dir('test*.tif');
+clear all;clc
+files=dir('*.tif');
 a=imread(files(1).name);
 minimg=uint16(4095*ones(size(a)));
+anzahl=25; %Anzahl Bilder zu mitteln
+n=numel(files)/anzahl; %Anzahl Mittelungen
 
-
-for k=1:numel(files)
-    if k==1
-    min1=minimg;
+for k=25:numel(files)-2
+    if k==25
+    min1=imread(files(k).name);
     end
     min1=min(min1,imread(files(k).name));
 end
@@ -24,15 +25,18 @@ parfor k=1:numel(files)
 end
 %PIV_base
 clear k
-for i=0:49
-for k=(1+i):50:numel(files)
+for i=0:(anzahl-1)
+for k=(1+i):anzahl:numel(files)
     if k==1+i
-    [xgrid,ygrid,uvecs,vvecs,peaks,valid,cmaps] = PIV_base (a1{k},a2{k},1,[64,64],[16,16],[0,0],[32,32],[],0);
+    [xgrid,ygrid,uvecs,vvecs,peaks,valid,cmaps] = PIV_base (a1{k},a2{k},1,[64,64],[10,10],[0,0],[32,32],[],0);
     else
-    [xgrid,ygrid,uvecs,vvecs,peaks,valid,cmaps] = PIV_base (a1{k},a2{k},1,[64,64],[16,16],[0,0],[32,32],cmaps,0);
+    [xgrid,ygrid,uvecs,vvecs,peaks,valid,cmaps] = PIV_base (a1{k},a2{k},1,[64,64],[10,10],[0,0],[32,32],cmaps,0);
     end
     
 end
     uvec{i+1}=uvecs;vvec{i+1}=vvecs;peak{i+1}=peaks;val{i+1}=valid;cmap{i+1}=cmaps;
 end
 clear i k
+for i=1:numel(peak)
+   peak{i}=peak{i}./n; 
+end
--- karl2.m
+++ karl2.m
@@ -1,8 +1,17 @@
-run karl
+% run karl
+% filename = 'analyse1.mat';
+% m = matfile(filename);
+% uvec=m.uvec;
+% vvec=m.vvec;
+% peak=m.peak;
+% xgrid=m.xgrid;
+% ygrid=m.ygrid;
+anzahl=m.anzahl;
 x=xgrid(1,length(xgrid(1,:)));
 y=ygrid(length(ygrid(:,1)),1);
 a=545/40; %[px/mm]
-dt=0.5; %[ms]
+dt=1; %[ms]
+
 clear l i k 
 %Pixel in Velocity [m/s]
 for i=1:numel(peak)
@@ -23,53 +32,63 @@ end
 end
 clear l i k
 %Filtering Vectordata
-for l=1:numel(peak)
-   for i=2:38
-       for k=2:30
-           if tot{l}(k,i)>=2*max(mean(tot{l}))
-            tot{l}(k,i)=median(median([tot{l}(k-1,i-1) tot{l}(k-1,i) tot{l}(k-1,i+1);...
-            tot{l}(k,i-1) tot{l}(k,i) tot{l}(k,i+1);tot{l}(k+1,i-1) tot{l}(k+1,i) tot{l}(k+1,i-1)]));
-           end
-            
-            phase{l}(k,i)=median(median([phase{l}(k-1,i-1) phase{l}(k-1,i) phase{l}(k-1,i+1);...
-            phase{l}(k,i-1) phase{l}(k,i) phase{l}(k,i+1);phase{l}(k+1,i-1) phase{l}(k+1,i) phase{l}(k+1,i-1)]));
-
-            uv{l}(k,i)=tot{l}(k,i).*cos(phase{l}(k,i));
-            vv{l}(k,i)=tot{l}(k,i).*sin(phase{l}(k,i));
-       end
-   end
-end
+% for l=1:numel(peak)
+%    for i=2:38
+%        for k=2:30
+% %            if tot{l}(k,i)>=2*max(mean(tot{l}))
+%             tot{l}(k,i)=median(median([tot{l}(k-1,i-1) tot{l}(k-1,i) tot{l}(k-1,i+1);...
+%             tot{l}(k,i-1) tot{l}(k,i) tot{l}(k,i+1);tot{l}(k+1,i-1) tot{l}(k+1,i) tot{l}(k+1,i-1)]));
+% %            end
+%             
+%             phase{l}(k,i)=median(median([phase{l}(k-1,i-1) phase{l}(k-1,i) phase{l}(k-1,i+1);...
+%             phase{l}(k,i-1) phase{l}(k,i) phase{l}(k,i+1);phase{l}(k+1,i-1) phase{l}(k+1,i) phase{l}(k+1,i-1)]));
+% 
+%             uv{l}(k,i)=tot{l}(k,i).*cos(phase{l}(k,i));
+%             vv{l}(k,i)=tot{l}(k,i).*sin(phase{l}(k,i));
+%        end
+%    end
+% end
 
 figure('units','normalized','outerposition',[0 0 1 1])
 
 for i=1:numel(peak)
-    md(i)=max(mean(tot{i}'));
+    md(i)=max(median(tot{i},2));
     mp(i)=mean(mean(phase{i}));
-    subplot(121)
-    quiver(xgrid,ygrid,uv{i},vv{i},1.6)
+%     subplot(121)
+    quiver(xgrid,ygrid,uv{i},vv{i})
     axis([0 x 0 y])
+%     drawnow
     F1(i)=getframe;
-    subplot(122)
-    contourf((tot{i}))
+%     subplot(122)
+    surf((tot{i}))
+    view(2)
+    colorbar
     colormap jet
+%     drawnow
     F2(i)=getframe;
 %     F3(i)=getframe;
     
 end
 figure('units','normalized','outerposition',[0 0 1 1])
 % subplot(121)
-t=0:0.00013:0.0064;
+t=0:0.0067/(anzahl):0.0067;
 s=-0.045*cos(2*pi*150*(t+0.005))+1.085;
 plot(s)
 hold on
-plot(1:numel(peak),smooth(smooth(smooth(md))))
+plot(1:numel(peak),smooth(smooth(md)))
 % subplot(122)
 
 avel=mean(md); %[m/s]
 
 figure('units','normalized','outerposition',[0 0 1 1])
-movie(F1,3,2)
-
-% movie(F2,1,3)
+ movie(F1,1,4)
+ movie(F2,1,4)
 % movie(F3,1,3)
 clear i l k
+% for i=1:numel(peak)
+%     [phi{i},psi{i}]=flowfun(uv{i},vv{i});
+%     imshow(cmap{i},[])
+% %     surf(peak{i}./5)
+%     view(2)
+%     drawnow
+% end
